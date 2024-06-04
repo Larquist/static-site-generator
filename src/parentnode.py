9491afdac0a68d6eccb745de019a0e9e26b0a152
larquist <evansgtravis@gmail.com>
2024-06-04 12:46:23 -0400

    completed ParentNode class w/ test script

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,8 +1,9 @@
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag = None, children = None, props =None):
-        super().__init__(tag, value, children, props)
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
         if self.tag == None:
@@ -11,4 +12,10 @@ class ParentNode(HTMLNode):
         if self.children == None:
             raise ValueError("Child values must be provided in a parent node")
 
-    
+        content = ""
+
+        for child in self.children:
+            content = content + child.to_html()
+
+        # <par><child>text</child><child>more text</child></par>
+        return f"<{self.tag + self.props_to_html()}>{content}</{self.tag}>"
---
+++ src/test_parentnode.py
@@ -0,0 +1,43 @@
+import unittest
+
+from parentnode import ParentNode
+from leafnode import LeafNode
+
+class TestParentNode(unittest.TestCase):
+    def test_eq(self):
+        # TEST #1 - Just Leaf nodes
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        expected = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+        self.assertEqual(node.to_html(), expected)
+        print("Parent node test #1:\n", 
+        "Expected:", expected, "\n"
+        "Actual:", node.to_html())
+
+        # TEST #2 - w/ Parent node inside
+        node2 = ParentNode(
+            "div",
+            [
+                LeafNode("b", "Bold text"),
+                node,
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        expected2 = "<div><b>Bold text</b><p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p><i>italic text</i>Normal text</div>"
+        self.assertEqual(node2.to_html(), expected2)
+        print("Parent node test #2:\n", 
+        "Expected:", expected2, "\n"
+        "Actual:", node2.to_html())
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
